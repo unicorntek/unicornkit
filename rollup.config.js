1315701093614fe9079c35abcc3ef9cc690c6af5
MeastBlue <amalou.massinissa@gmail.com>
2023-03-19 02:24:54 +0100

    update config

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,34 @@
-import typescript from "@rollup/plugin-typescript"
+import commonjs from "@rollup/plugin-commonjs"
+import dts from "rollup-plugin-dts"
+import resolve from "@rollup/plugin-node-resolve"
 import { terser } from "rollup-plugin-terser"
+import typescript from "@rollup/plugin-typescript"
 
-export default {
-  input: "src/index.ts",
-  output: [
-    {
-      file: "dist/cjs/index.js",
-      format: "cjs",
-      sourcemap: true,
-    },
-    {
-      file: "dist/esm/index.js",
-      format: "esm",
-      sourcemap: true,
-    },
-  ],
-  plugins: [typescript(), terser()],
-}
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: "dist/cjs/index.js",
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: "dist/esm/index.js",
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      commonjs(),
+      resolve(),
+      terser(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+    ],
+  },
+  {
+    input: "dist/esm/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+  },
+]
